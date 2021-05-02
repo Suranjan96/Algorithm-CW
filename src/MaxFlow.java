068c0dc37fc539ad8d21d01922246d090f125717
Suranjan Kandamudali <61134465+Suranjan96@users.noreply.github.com>
2021-05-02 08:55:53 +0530

    add augmenting path to display

--- src/MaxFlow.java
+++ src/MaxFlow.java
@@ -14,17 +14,17 @@ class MaxFlow {                                                 //https://www.ge
         LinkedList<Integer> queue = new LinkedList<Integer>();              // Create a queue, enqueue source vertex and mark
         queue.add(s);                                                       // source vertex as visited
         visited[s] = true;
-        parent[s] = -1;    //TODO
+        //parent[s] = -1;    //TODO
 
         while (queue.size() != 0) {                                         // Standard BFS Loop
             int u = queue.poll();
             for (int v = 0; v < graph.getNumOfNode(); v++) {
                 if (visited[v] == false && rGraph[u][v] > 0) {              // If we find a connection to the sink node, then there is no point in BFS anymore We just have to set its parent and can return true
-                    if (v == t) {
-                        parent[v] = u;
+                    if (v == t) {                                           //check is there path
+                        parent[v] = u;                                      //store the path
                         return true;
                     }
-                    queue.add(v);
+                    queue.add(v);                                           //add traveled path
                     parent[v] = u;
                     visited[v] = true;
                 }
@@ -44,7 +44,7 @@ class MaxFlow {                                                 //https://www.ge
             int parent[] = new int[MaxFlow.graph.getNumOfNode()];           // This array is filled by BFS and to store path
         int max_flow = 0;                                                   // There is no flow initially
 
-        while (bfs(rGraph, s, t, parent)) {                                 // Augment the flow while tere is path from source to sink
+        while (bfs(rGraph, s, t, parent)) {                                 // Augment the flow while there is path from source to sink
             int path_flow = Integer.MAX_VALUE;                      // Find minimum residual capacity of the edges along the path filled by BFS. Or we can say find the maximum flow through the path found.
             for (v = t; v != s; v = parent[v]) {
                 u = parent[v];
@@ -55,29 +55,34 @@ class MaxFlow {                                                 //https://www.ge
                 u = parent[v];
                 rGraph[u][v] -= path_flow;
                 rGraph[v][u] += path_flow;
+
             }
+            System.out.println("Augmenting Path "+ path_flow);
             max_flow += path_flow;                  // Add path flow to overall flow
         }
         return max_flow;            // Return the overall flow
     }
 
-    public static void getDataset() throws FileNotFoundException{
-        File file = new File("Data set/ladder_1.txt");          //read the file
-        Scanner scanner = new Scanner(file);
-        String[] capArray = scanner.nextLine().split(" ");          //add all data in to string array and check the first element
-        int numOfNode = Integer.parseInt(capArray[0]);
-        graph = new Graph(numOfNode);                       //pass the variable to the constructor
+    public static void getDataset(){
+       try{
+           File file = new File("Data set/test1.txt");          //read the file
+           Scanner scanner = new Scanner(file);
+           String[] capArray = scanner.nextLine().split(" ");          //add all data in to string array and check the first element
+           int numOfNode = Integer.parseInt(capArray[0]);
+           graph = new Graph(numOfNode);                       //pass the variable to the constructor
 
-        while (scanner.hasNextLine()){
-            String[] array = scanner.nextLine().split(" ");         //add all data from file and check all elements to the order
-            int u = Integer.parseInt(array[0]);                 //source
-            int v = Integer.parseInt(array[1]);                 //target
-            int cap = Integer.parseInt(array[2]);               //capacity
-           graph.addEdges(u,v,cap);
-        }
-    }
+           while (scanner.hasNextLine()){
+               String[] array = scanner.nextLine().split(" ");         //add all data from file and check all elements to the order
+               int u = Integer.parseInt(array[0]);                 //source
+               int v = Integer.parseInt(array[1]);                 //target
+               int cap = Integer.parseInt(array[2]);               //capacity
+               graph.addEdges(u,v,cap);
+           }
+       }catch (FileNotFoundException e){
+           System.out.println("File not Found");
 
-
+       }
+   }
 
     public static void main(String[] args) throws java.lang.Exception {             // Driver program to test above functions
         double []averageTime = new double[3];           //double array to get average time
@@ -85,7 +90,7 @@ class MaxFlow {                                                 //https://www.ge
         Stopwatch stopwatch = new Stopwatch();
 
         getDataset();
-        System.out.println("The maximum possible flow is " + m.fordFulkerson(graph.getAddMatrix(), 0, graph.getNumOfNode()-1));
+        System.out.println("\n"+"The maximum possible flow is " + m.fordFulkerson(graph.getAddMatrix(), 0, graph.getNumOfNode()-1));
         for(int i =1; i<=3;i++){
             System.out.println("Time "+i+" : "+stopwatch.elapsedTime());
             averageTime[i-1]= stopwatch.elapsedTime();
